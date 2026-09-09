07b7fc789773fc850d5f6e8715e2dc2763bc4647
Sqqur000 <s2036164@gmail.com>
2026-09-09 19:27:06 +0300

    updateEnv

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/homeScreen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async{
+Future<void> main() async{
+  await dotenv.load(); 
    WidgetsFlutterBinding.ensureInitialized();
-
-  await Supabase.initialize(
-    url: 'https://fxfjpgembctxczacvzyp.supabase.co',
-    publishableKey: 'my key',
+   
+    await Supabase.initialize(
+    url: dotenv.get('url'),
+    publishableKey: dotenv.get('publishableKey'),
   );
+   
   runApp(const MainApp());
 }
 
